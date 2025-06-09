2461903fbbcb1e86fef67c945c7ee1bcdddb9019
KphStar <37385165+KphStar@users.noreply.github.com>
2025-06-08 19:15:26 -0700

    chage the chart price to look more like robinhood

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,151 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lablAccountNumber = new System.Windows.Forms.Label();
+            this.lablStaus = new System.Windows.Forms.Label();
+            this.lablBuyingPower = new System.Windows.Forms.Label();
+            this.chartprice = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.priceTimer = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartprice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(1200, 678);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(156, 41);
+            this.BtnExit.TabIndex = 0;
+            this.BtnExit.Text = "EXIT";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Fluent Icons", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(390, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(463, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "WELCOME TO CRYPTO WATCHING";
+            // 
+            // lablAccountNumber
+            // 
+            this.lablAccountNumber.AutoSize = true;
+            this.lablAccountNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lablAccountNumber.Location = new System.Drawing.Point(714, 53);
+            this.lablAccountNumber.Name = "lablAccountNumber";
+            this.lablAccountNumber.Size = new System.Drawing.Size(128, 20);
+            this.lablAccountNumber.TabIndex = 4;
+            this.lablAccountNumber.Text = "Account Number";
+            // 
+            // lablStaus
+            // 
+            this.lablStaus.AutoSize = true;
+            this.lablStaus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lablStaus.Location = new System.Drawing.Point(809, 73);
+            this.lablStaus.Name = "lablStaus";
+            this.lablStaus.Size = new System.Drawing.Size(44, 16);
+            this.lablStaus.TabIndex = 5;
+            this.lablStaus.Text = "Status";
+            // 
+            // lablBuyingPower
+            // 
+            this.lablBuyingPower.AutoSize = true;
+            this.lablBuyingPower.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lablBuyingPower.Location = new System.Drawing.Point(390, 99);
+            this.lablBuyingPower.Name = "lablBuyingPower";
+            this.lablBuyingPower.Size = new System.Drawing.Size(193, 31);
+            this.lablBuyingPower.TabIndex = 6;
+            this.lablBuyingPower.Text = "Buying Power";
+            // 
+            // chartprice
+            // 
+            this.chartprice.BackColor = System.Drawing.Color.Gray;
+            this.chartprice.BorderlineColor = System.Drawing.Color.DimGray;
+            chartArea1.Name = "ChartArea1";
+            this.chartprice.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartprice.Legends.Add(legend1);
+            this.chartprice.Location = new System.Drawing.Point(15, 196);
+            this.chartprice.Name = "chartprice";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.Legend = "Legend1";
+            series1.Name = "ChartPrice";
+            this.chartprice.Series.Add(series1);
+            this.chartprice.Size = new System.Drawing.Size(1341, 476);
+            this.chartprice.TabIndex = 8;
+            this.chartprice.Text = "chart1";
+            this.chartprice.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chartprice_MouseMove);
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(236, 138);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(57, 55);
+            this.lblPrice.TabIndex = 9;
+            this.lblPrice.Text = "P";
+            // 
+            // priceTimer
+            // 
+            this.priceTimer.Tick += new System.EventHandler(this.priceTimer_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 55);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "BITCOIN:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1368, 731);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.chartprice);
+            this.Controls.Add(this.lablBuyingPower);
+            this.Controls.Add(this.lablStaus);
+            this.Controls.Add(this.lablAccountNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartprice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lablAccountNumber;
+        private System.Windows.Forms.Label lablStaus;
+        private System.Windows.Forms.Label lablBuyingPower;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartprice;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Timer priceTimer;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Security;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +21,240 @@ namespace Robinhood_API
 {
     public partial class Form1 : Form
     {
+
+        // GLOBAL CONSTANTS FILE FOLDER PATH 
+
+        private readonly string keyPath = @"C:\Users\USER PC\Documents\C#\Robinhood_API";
+        private readonly string privateKeyFile = "my_private_key.txt";
+        private readonly string robinhoodApiKey = "RobKeys.txt"; // <-- Your Robinhood API Key
         public Form1()
         {
             InitializeComponent();
         }
+
+        private async void GetInfo()
+        {
+            await CallAndDisplayResult(async client => await client.GetAccountInfoAsync());
+        }
+
+
+        // -------- UTILITY TO CREATE CLIENT & DISPLAY --------
+
+        private async Task CallAndDisplayResult(Func<RobinhoodApiClient, Task<string>> action)
+        {
+            try
+            {
+                string privateKeyBase64 = File.ReadAllText(Path.Combine(keyPath, privateKeyFile));
+                string apiKeyString = File.ReadAllText(Path.Combine(keyPath, robinhoodApiKey)).Trim();
+                var client = new RobinhoodApiClient(apiKeyString, privateKeyBase64);
+
+                string result = await action(client);
+
+                // Parse the JSON and set labels
+                var j = JObject.Parse(result);
+                lablAccountNumber.Text = $"Account Number: {j["account_number"]?.ToString() ?? "N/A"}";
+                lablStaus.Text = $"Status: {j["status"]?.ToString() ?? "N/A"}";
+                lablBuyingPower.Text = $"Buying Power: {j["buying_power"]?.ToString() ?? "N/A"} {j["buying_power_currency"]?.ToString() ?? ""}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error");
+            }
+        }
+
+        private void ChartSetup()
+        {
+            // Chart setup code can be added here if needed
+            chartprice.Series.Clear();
+            chartprice.Series.Add("BTC-USD");
+            chartprice.Series["BTC-USD"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chartprice.Series["BTC-USD"].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            chartprice.Series["BTC-USD"].BorderWidth = 4; 
+            chartprice.ChartAreas[0].AxisX.LabelStyle.Format = "HH:mm:ss";
+            chartprice.ChartAreas[0].AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Seconds;
+
+            chartprice.Series.Add("Marker");
+            chartprice.Series["Marker"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            chartprice.Series["Marker"].MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            chartprice.Series["Marker"].MarkerSize = 10;
+            chartprice.Series["Marker"].Color = Color.Yellow;
+            chartprice.Series["Marker"].IsVisibleInLegend = false;
+
+            // ---- Remove grid and set transparent background ----
+            var area = chartprice.ChartAreas[0];
+            area.AxisX.LabelStyle.Enabled = false; // <---- Hides bottom numbers
+            area.AxisY.LabelStyle.Enabled = false; // <---- Hides left numbers
+            area.AxisX.LineWidth = 0;
+            area.AxisY.LineWidth = 0;
+            area.AxisX.MajorTickMark.Enabled = false;
+            area.AxisY.MajorTickMark.Enabled = false;
+            area.AxisX.MajorGrid.Enabled = false;
+            area.AxisY.MajorGrid.Enabled = false;
+            area.AxisX.MinorGrid.Enabled = false;
+            area.AxisY.MinorGrid.Enabled = false;
+            chartprice.BackColor = Color.Transparent;
+            area.BackColor = Color.Transparent;
+            area.BackSecondaryColor = Color.Transparent;
+            area.ShadowColor = Color.Transparent;
+
+            // Hide chart legend/label
+            chartprice.Legends[0].Enabled = false;
+
+
+            // Timer setup
+            priceTimer.Interval = 1000; // 1 second
+            priceTimer.Tick += priceTimer_Tick;
+            priceTimer.Start();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetInfo();
+            ChartSetup();
+        }
+
+        private void chartprice_MouseMove(object sender, MouseEventArgs e)
+        {
+            var area = chartprice.ChartAreas[0];
+            var series = chartprice.Series["BTC-USD"];
+            if (series.Points.Count == 0) return;
+
+            // Convert mouse X to chart X (OADate/time)
+            double mouseXVal = area.AxisX.PixelPositionToValue(e.X);
+
+            // Find nearest point (by XValue, i.e., time)
+            var nearest = series.Points
+                .OrderBy(pt => Math.Abs(pt.XValue - mouseXVal))
+                .First();
+
+            DateTime time = DateTime.FromOADate(nearest.XValue);
+            double price = nearest.YValues[0];
+
+            // Update marker dot
+            var markerSeries = chartprice.Series["Marker"];
+            markerSeries.Points.Clear();
+            markerSeries.Points.AddXY(nearest.XValue, price);
+
+            // Update label (price changes as you move along the line!)
+            lblPrice.Text = $"Time: {time:HH:mm:ss} | Price: {price:N2}";
+        }
+
+        private async void priceTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                string privateKeyBase64 = File.ReadAllText(Path.Combine(keyPath, privateKeyFile));
+                string apiKeyString = File.ReadAllText(Path.Combine(keyPath, robinhoodApiKey)).Trim();
+
+                var client = new RobinhoodApiClient(apiKeyString, privateKeyBase64);
+                string result = await client.GetBestBidAskAsync("BTC-USD");
+
+                var j = JObject.Parse(result);
+                var arr = j["results"] as JArray;
+                if (arr != null && arr.Count > 0)
+                {
+                    var priceStr = arr[0]["price"]?.ToString();
+                    decimal price = decimal.TryParse(priceStr, out var p) ? p : 0m;
+                    DateTime now = DateTime.Now;
+
+                    chartprice.Series["BTC-USD"].Points.AddXY(now, price);
+
+                    if (chartprice.Series["BTC-USD"].Points.Count > 100)
+                        chartprice.Series["BTC-USD"].Points.RemoveAt(0);
+
+                    var points = chartprice.Series["BTC-USD"].Points;
+                    if (points.Count > 0)
+                    {
+                        chartprice.ChartAreas[0].AxisX.Minimum = points[0].XValue;
+                        chartprice.ChartAreas[0].AxisX.Maximum = points[points.Count - 1].XValue;
+
+                        // === Auto-zoom Y-axis ===
+                        double minY = points.Select(pt => pt.YValues[0]).Min();
+                        double maxY = points.Select(pt => pt.YValues[0]).Max();
+                        double margin = (maxY - minY) * 0.05;
+                        if (margin == 0) margin = maxY * 0.01;
+                        chartprice.ChartAreas[0].AxisY.Minimum = minY - margin;
+                        chartprice.ChartAreas[0].AxisY.Maximum = maxY + margin;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error fetching price: " + ex.Message, "Error");
+            }
+        }
+    }
+    
+}
+
+// ------------ Robinhood API CLIENT ------------
+
+public class RobinhoodApiClient
+{
+    private readonly string _apiKey;
+    private readonly Ed25519PrivateKeyParameters _privateKey;
+    private readonly HttpClient _client = new HttpClient();
+    private readonly string _baseUrl = "https://trading.robinhood.com";
+
+    public RobinhoodApiClient(string apiKey, string privateKeyBase64)
+    {
+        _apiKey = apiKey;
+        var privateKeyBytes = Convert.FromBase64String(privateKeyBase64);
+        _privateKey = new Ed25519PrivateKeyParameters(privateKeyBytes, 0);
+    }
+
+    private string CreateSignature(string message)
+    {
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        var signer = new Ed25519Signer();
+        signer.Init(true, _privateKey);
+        signer.BlockUpdate(messageBytes, 0, messageBytes.Length);
+        var signature = signer.GenerateSignature();
+        return Convert.ToBase64String(signature);
+    }
+
+    // ---- Generic GET ----
+    private async Task<string> SendRobinhoodGetRequestAsync(string path)
+    {
+        string method = "GET";
+        string body = "";
+        long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+        string message = $"{_apiKey}{timestamp}{path}{method}{body}";
+        string signature = CreateSignature(message);
+
+        var request = new HttpRequestMessage(HttpMethod.Get, _baseUrl + path);
+        request.Headers.Add("x-api-key", _apiKey);
+        request.Headers.Add("x-timestamp", timestamp.ToString());
+        request.Headers.Add("x-signature", signature);
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var response = await _client.SendAsync(request);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    // ---- Endpoints ----
+
+    public async Task<string> GetAccountInfoAsync()
+    {
+        string path = "/api/v1/crypto/trading/accounts/";
+        return await SendRobinhoodGetRequestAsync(path);
+    }
+
+    public async Task<string> GetBestBidAskAsync(params string[] symbols)
+    {
+        string path = "/api/v1/crypto/marketdata/best_bid_ask/";
+        if (symbols != null && symbols.Length > 0)
+        {
+            var query = string.Join("&", symbols.Select(s => $"symbol={Uri.EscapeDataString(s.ToUpper())}"));
+            path += "?" + query;
+        }
+        return await SendRobinhoodGetRequestAsync(path);
     }
 }
+    
